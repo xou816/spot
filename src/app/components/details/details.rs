aeeb6b44ee9c561d1103363010b0434566250624
Alexandre Trendel <trendel.alexandre@gmail.com>
2020-12-23 13:58:08 +0100

    details

--- src/app/components/details/details.rs
+++ src/app/components/details/details.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 use gtk::prelude::*;
-use gtk::{RevealerExt, LinkButtonExt};
+use gtk::LinkButtonExt;
 use gladis::Gladis;
 
 use crate::app::components::{Component, EventListener, screen_add_css_provider, PlaylistFactory};
@@ -12,13 +12,11 @@ use super::DetailsModel;
 #[derive(Gladis, Clone)]
 struct DetailsWidget {
     pub root: gtk::Widget,
-    pub artist_label: gtk::Label,
     pub album_label: gtk::Label,
     pub album_tracks: gtk::ListBox,
-    pub album_art_revealer: gtk::Revealer,
     pub album_art: gtk::Image,
     pub like_button: gtk::Button,
-    pub more_button: gtk::LinkButton
+    pub artist_button: gtk::LinkButton
 }
 
 impl DetailsWidget {
@@ -26,6 +24,11 @@ impl DetailsWidget {
     fn new() -> Self {
         screen_add_css_provider(resource!("/components/details.css"));
         Self::from_resource(resource!("/components/details.ui")).unwrap()
+    }
+
+    fn set_loaded(&self) {
+        let context = self.root.get_style_context();
+        context.add_class("details--loaded");
     }
 }
 
@@ -54,33 +57,30 @@ impl Details {
     fn update_details(&self) {
         if let Some(info) = self.model.get_album_info() {
 
-            let context = self.widget.root.get_style_context();
-            context.add_class("details--loaded");
-
             let album = &info.title[..];
             let artist = &info.artist[..];
             let art = info.art.clone();
             let is_liked = false;
 
             self.widget.album_label.set_label(album);
-            self.widget.artist_label.set_label(artist);
             self.widget.like_button.set_label(if is_liked { "♥" } else { "♡" });
+            self.widget.artist_button.set_label(artist);
+
 
             let weak_model = Rc::downgrade(&self.model);
-            self.widget.more_button.connect_activate_link(move |_| {
+            self.widget.artist_button.connect_activate_link(move |_| {
                 if let Some(model) = weak_model.upgrade() {
                     model.view_artist();
                 }
                 glib::signal::Inhibit(true)
             });
 
-            let revealer = self.widget.album_art_revealer.clone();
-            let image = self.widget.album_art.clone();
+            let widget = self.widget.clone();
             self.worker.send_local_task(async move {
                 let pixbuf = ImageLoader::new()
                     .load_remote(&art[..], "jpg", 100, 100).await;
-                image.set_from_pixbuf(pixbuf.as_ref());
-                revealer.set_reveal_child(true);
+                widget.album_art.set_from_pixbuf(pixbuf.as_ref());
+                widget.set_loaded();
             });
         }
     }
