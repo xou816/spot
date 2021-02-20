6191c5a1cf5cd828200ae54e789b58f64450f8c9
Tom <25043847+Douile@users.noreply.github.com>
2021-02-20 18:24:32 +0000

    Fix/Improve keyboard navigation for login dialog (#78)
    * Fix bad can-focus attributes
    * Submit login when the user presses enter

--- src/app/components/login/login.rs
+++ src/app/components/login/login.rs
@@ -1,4 +1,6 @@
+use gdk::EventKey;
 use gio::ApplicationExt;
+use glib::translate::ToGlib;
 use gtk::prelude::*;
 use gtk::{EntryExt, GtkWindowExt, WidgetExt};
 use std::rc::Rc;
@@ -8,6 +10,8 @@ use crate::app::credentials::Credentials;
 use crate::app::AppEvent;
 
 use super::LoginModel;
+
+const SUBMIT_KEY: &str = "Return";
 
 pub struct Login {
     dialog: gtk::Dialog,
@@ -27,18 +31,26 @@ impl Login {
         let model = Rc::new(model);
         login_btn.connect_clicked(
             clone!(@weak username, @weak password,  @weak model => move |_| {
-                let username = username.get_text().as_str().to_string();
-                let password = password.get_text().as_str().to_string();
-                model.login(username, password);
+                Login::submit_login_form(username, password, model);
+            }),
+        );
+        username.connect_key_press_event(
+            clone!(@weak username, @weak password, @weak model => @default-return Inhibit(false), move |_, event | {
+                Login::handle_keypress(username, password, model, event)
+            }),
+        );
+        password.connect_key_press_event(
+            clone!(@weak username, @weak password, @weak model => @default-return Inhibit(false), move |_, event | {
+                Login::handle_keypress(username, password, model, event)
             }),
         );
 
         dialog.connect_delete_event(
-            clone!(@weak parent => @default-return gtk::Inhibit(false), move |_, _| {
+            clone!(@weak parent => @default-return Inhibit(false), move |_, _| {
                 if let Some(app) = parent.get_application().as_ref() {
                     app.quit();
                 }
-                gtk::Inhibit(true)
+                Inhibit(true)
             }),
         );
 
@@ -66,6 +78,32 @@ impl Login {
     fn hide_and_save_creds(&self, credentials: Credentials) {
         self.dialog.hide();
         self.model.save_for_autologin(credentials);
+    }
+
+    fn handle_keypress(
+        username: gtk::Entry,
+        password: gtk::Entry,
+        model: Rc<LoginModel>,
+        event: &EventKey,
+    ) -> Inhibit {
+        if event.get_keyval().to_glib() == gdk::keyval_from_name(SUBMIT_KEY) {
+            Login::submit_login_form(username, password, model);
+            Inhibit(true)
+        } else {
+            Inhibit(false)
+        }
+    }
+
+    fn submit_login_form(username: gtk::Entry, password: gtk::Entry, model: Rc<LoginModel>) {
+        let username_text = username.get_text().as_str().to_string();
+        let password_text = password.get_text().as_str().to_string();
+        if username_text.is_empty() {
+            username.grab_focus();
+        } else if password_text.is_empty() {
+            password.grab_focus();
+        } else {
+            model.login(username_text, password_text);
+        }
     }
 }
 
