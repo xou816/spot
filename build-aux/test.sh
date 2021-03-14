65a02613cc96d660898434249260230f1cb757d2
Daniel Peukert <daniel@peukert.cc>
2021-03-14 17:48:11 +0100

    Take buildtype into account when testing

--- build-aux/test.sh
+++ build-aux/test.sh
@@ -2,10 +2,19 @@
 
 export SRC="$1"
 export CARGO_TARGET_DIR="$2"/target
-export OFFLINE="$3"
+export BUILDTYPE="$3"
+export OFFLINE="$4"
+
+echo $BUILDTYPE
+
+if [[ $BUILDTYPE = "release" ]]; then
+    PROFILE_ARG="--release"
+else
+    PROFILE_ARG="--verbose"
+fi
 
 if [[ $OFFLINE = "true" ]]; then
     export CARGO_HOME="$SRC"/cargo
 fi
 
-cargo test --manifest-path "$SRC"/Cargo.toml
+cargo test --manifest-path "$SRC"/Cargo.toml "$PROFILE_ARG"
