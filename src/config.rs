fbee08656aae257c29a43b0c7da212a162b56389
Alexandre Trendel <trendel.alexandre@gmail.com>
2021-03-24 13:26:33 +0100

    0.1.12

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
 pub static PKGDATADIR: &str = "/home/alexandre/.local/share/spot";
-pub static VERSION: &str = "0.1.11";
+pub static VERSION: &str = "0.1.12";
 pub static LOCALEDIR: &str = "/home/alexandre/.local/share/locale";
