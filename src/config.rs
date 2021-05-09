40eccfdb8c9fc823c1985250e3364a162bf69d54
Alexandre Trendel <trendel.alexandre@gmail.com>
2021-05-09 15:33:46 +0200

    0.1.14

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static PKGDATADIR: &str = "/home/alexandre/.local/share/spot";
-pub static VERSION: &str = "0.1.13-dev";
+pub static VERSION: &str = "0.1.14-dev";
 pub static LOCALEDIR: &str = "/home/alexandre/.local/share/locale";
 pub static APPID: &str = "dev.alextren.Spot.Devel";
