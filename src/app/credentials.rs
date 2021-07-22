84a57c3860285fb9c3c42ddc72aa194f082ed37e
Niklas Sauter <niklas@n1ks.net>
2021-07-22 17:23:49 +0200

    Use default serializer/deserializer for `SystemTime`

--- src/app/credentials.rs
+++ src/app/credentials.rs
@@ -1,7 +1,6 @@
 use secret_service::{EncryptionType, Error, SecretService};
-use serde::{de::Error as _, ser::Error as _, Deserialize, Deserializer, Serialize, Serializer};
-use std::collections::HashMap;
-use std::time::{Duration, SystemTime};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, time::SystemTime};
 
 static SPOT_ATTR: &str = "spot_credentials";
 
@@ -11,43 +10,11 @@ fn make_attributes() -> HashMap<&'static str, &'static str> {
     attributes
 }
 
-mod serde_unix_timestamp {
-    use super::*;
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<SystemTime>, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        match <Option<u64>>::deserialize(deserializer)? {
-            Some(v) => Ok(Some(
-                SystemTime::UNIX_EPOCH
-                    .checked_add(Duration::from_secs(v))
-                    .ok_or_else(|| D::Error::custom("overflow deserializing SystemTime"))?,
-            )),
-            None => Ok(None),
-        }
-    }
-
-    pub fn serialize<S>(system_time: &Option<SystemTime>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        system_time
-            .map(|v| match v.duration_since(SystemTime::UNIX_EPOCH) {
-                Ok(v) => Ok(v.as_secs()),
-                Err(_) => Err(S::Error::custom("SystemTime must be later than UNIX_EPOCH")),
-            })
-            .transpose()?
-            .serialize(serializer)
-    }
-}
-
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Credentials {
     pub username: String,
     pub password: String,
     pub token: String,
-    #[serde(with = "serde_unix_timestamp")]
     pub token_expiry_time: Option<SystemTime>,
     pub country: String,
 }
