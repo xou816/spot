36730066cdd3bf7b2f534afc90f5e43945594865
TotalDarkness-NRF <totaldarkness@totaldarkness.net>
2021-07-31 01:20:59 -0400

    Add repeat button actions

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -203,6 +203,7 @@ impl App {
         let play_button: gtk::Button = builder.object("play_pause").unwrap();
         let next: gtk::Button = builder.object("next").unwrap();
         let prev: gtk::Button = builder.object("prev").unwrap();
+        let repeat_button: gtk::Button = builder.object("repeat").unwrap();
         let seek_bar: gtk::Scale = builder.object("seek_bar").unwrap();
         let track_position: gtk::Label = builder.object("track_position").unwrap();
         let track_duration: gtk::Label = builder.object("track_duration").unwrap();
@@ -214,6 +215,7 @@ impl App {
             track_duration,
             next,
             prev,
+            repeat_button,
         );
 
         let model = PlaybackControlModel::new(app_model, dispatcher);
