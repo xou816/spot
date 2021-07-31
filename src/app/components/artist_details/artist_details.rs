15123bd26441c8b932ec0353fd8d61255e9c17c9
Alexandre Trendel <trendel.alexandre@gmail.com>
2021-07-31 18:27:48 +0200

    more composite templates

--- src/app/components/artist_details/artist_details.rs
+++ src/app/components/artist_details/artist_details.rs
@@ -1,27 +1,107 @@
-use gladis::Gladis;
 use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::CompositeTemplate;
 use std::rc::Rc;
 
 use crate::app::components::{
     screen_add_css_provider, AlbumWidget, Component, EventListener, Playlist,
 };
-use crate::app::models::*;
+use crate::app::{models::*, ListStore};
 use crate::app::{AppEvent, BrowserEvent, Worker};
 
 use super::ArtistDetailsModel;
 
-#[derive(Clone, Gladis)]
-struct ArtistDetailsWidget {
-    pub root: gtk::ScrolledWindow,
-    pub artist_name: gtk::Label,
-    pub top_tracks: gtk::ListBox,
-    pub artist_releases: gtk::FlowBox,
+mod imp {
+
+    use super::*;
+
+    #[derive(Debug, Default, CompositeTemplate)]
+    #[template(resource = "/dev/alextren/Spot/components/artist_details.ui")]
+    pub struct ArtistDetailsWidget {
+        #[template_child]
+        pub scrolled_window: TemplateChild<gtk::ScrolledWindow>,
+
+        #[template_child]
+        pub artist_name: TemplateChild<gtk::Label>,
+
+        #[template_child]
+        pub top_tracks: TemplateChild<gtk::ListBox>,
+
+        #[template_child]
+        pub artist_releases: TemplateChild<gtk::FlowBox>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for ArtistDetailsWidget {
+        const NAME: &'static str = "ArtistDetailsWidget";
+        type Type = super::ArtistDetailsWidget;
+        type ParentType = gtk::Box;
+
+        fn class_init(klass: &mut Self::Class) {
+            Self::bind_template(klass);
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for ArtistDetailsWidget {}
+    impl WidgetImpl for ArtistDetailsWidget {}
+    impl BoxImpl for ArtistDetailsWidget {}
+}
+
+glib::wrapper! {
+    pub struct ArtistDetailsWidget(ObjectSubclass<imp::ArtistDetailsWidget>) @extends gtk::Widget, gtk::Box;
 }
 
 impl ArtistDetailsWidget {
     fn new() -> Self {
         screen_add_css_provider(resource!("/components/artist_details.css"));
-        Self::from_resource(resource!("/components/artist_details.ui")).unwrap()
+        glib::Object::new(&[]).expect("Failed to create an instance of ArtistDetailsWidget")
+    }
+
+    fn widget(&self) -> &imp::ArtistDetailsWidget {
+        imp::ArtistDetailsWidget::from_instance(self)
+    }
+
+    fn top_tracks_widget(&self) -> &gtk::ListBox {
+        self.widget().top_tracks.as_ref()
+    }
+
+    fn scrolled_window_widget(&self) -> &gtk::ScrolledWindow {
+        self.widget().scrolled_window.as_ref()
+    }
+
+    fn set_artist_name(&self, name: &str) {
+        let context = self.style_context();
+        context.add_class("artist__loaded");
+        self.widget().artist_name.set_text(name);
+    }
+
+    fn bind_artist_releases<F>(
+        &self,
+        worker: Worker,
+        store: &ListStore<AlbumModel>,
+        on_album_pressed: F,
+    ) where
+        F: Fn(&String) + Clone + 'static,
+    {
+        self.widget()
+            .artist_releases
+            .bind_model(Some(store.unsafe_store()), move |item| {
+                let item = item.downcast_ref::<AlbumModel>().unwrap();
+                let child = gtk::FlowBoxChild::new();
+                let album = AlbumWidget::for_model(item, worker.clone());
+                let f = on_album_pressed.clone();
+                album.connect_album_pressed(clone!(@weak item => move |_| {
+                    if let Some(id) = item.uri().as_ref() {
+                        f(id);
+                    }
+                }));
+                child.set_child(Some(&album));
+                child.upcast::<gtk::Widget>()
+            });
     }
 }
 
@@ -38,34 +118,28 @@ impl ArtistDetails {
         let widget = ArtistDetailsWidget::new();
 
         let weak_model = Rc::downgrade(&model);
-        widget.root.connect_edge_reached(move |_, pos| {
-            if let (gtk::PositionType::Bottom, Some(model)) = (pos, weak_model.upgrade()) {
-                let _ = model.load_more();
-            }
-        });
+        widget
+            .scrolled_window_widget()
+            .connect_edge_reached(move |_, pos| {
+                if let (gtk::PositionType::Bottom, Some(model)) = (pos, weak_model.upgrade()) {
+                    let _ = model.load_more();
+                }
+            });
 
         if let Some(store) = model.get_list_store() {
-            let model_clone = Rc::clone(&model);
-
-            widget
-                .artist_releases
-                .bind_model(Some(store.unsafe_store()), move |item| {
-                    let item = item.downcast_ref::<AlbumModel>().unwrap();
-                    let child = gtk::FlowBoxChild::new();
-                    let album = AlbumWidget::for_model(item, worker.clone());
-                    album.connect_album_pressed(
-                        clone!(@weak model_clone as model, @weak item => move |_| {
-                            if let Some(id) = item.uri().as_ref() {
-                                model.open_album(id);
-                            }
-                        }),
-                    );
-                    child.set_child(Some(&album));
-                    child.upcast::<gtk::Widget>()
-                });
+            widget.bind_artist_releases(
+                worker,
+                &*store,
+                clone!(@weak model => move |id| {
+                    model.open_album(id);
+                }),
+            );
         }
 
-        let playlist = Box::new(Playlist::new(widget.top_tracks.clone(), Rc::clone(&model)));
+        let playlist = Box::new(Playlist::new(
+            widget.top_tracks_widget().clone(),
+            Rc::clone(&model),
+        ));
 
         Self {
             model,
@@ -76,16 +150,14 @@ impl ArtistDetails {
 
     fn update_details(&mut self) {
         if let Some(name) = self.model.get_artist_name() {
-            let context = self.widget.root.style_context();
-            context.add_class("artist__loaded");
-            self.widget.artist_name.set_text(&name);
+            self.widget.set_artist_name(&name);
         }
     }
 }
 
 impl Component for ArtistDetails {
     fn get_root_widget(&self) -> &gtk::Widget {
-        self.widget.root.upcast_ref()
+        self.widget.upcast_ref()
     }
 
     fn get_children(&mut self) -> Option<&mut Vec<Box<dyn EventListener>>> {
--- src/app/components/details/details.rs
+++ src/app/components/details/details.rs
@@ -1,5 +1,6 @@
-use gladis::Gladis;
 use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::CompositeTemplate;
 use std::rc::Rc;
 
 use super::DetailsModel;
@@ -9,33 +10,134 @@ use crate::app::dispatch::Worker;
 use crate::app::loader::ImageLoader;
 use crate::app::{AppEvent, BrowserEvent};
 
-#[derive(Gladis, Clone)]
-struct DetailsWidget {
-    pub root: gtk::Widget,
-    pub album_label: gtk::Label,
-    pub album_tracks: gtk::ListBox,
-    pub album_art: gtk::Image,
-    pub like_button: gtk::Button,
-    pub artist_button: gtk::LinkButton,
-    pub artist_button_label: gtk::Label,
+mod imp {
+
+    use super::*;
+
+    #[derive(Debug, Default, CompositeTemplate)]
+    #[template(resource = "/dev/alextren/Spot/components/details.ui")]
+    pub struct AlbumDetailsWidget {
+        #[template_child]
+        pub root: TemplateChild<gtk::Widget>,
+
+        #[template_child]
+        pub album_label: TemplateChild<gtk::Label>,
+
+        #[template_child]
+        pub album_tracks: TemplateChild<gtk::ListBox>,
+
+        #[template_child]
+        pub album_art: TemplateChild<gtk::Image>,
+
+        #[template_child]
+        pub like_button: TemplateChild<gtk::Button>,
+
+        #[template_child]
+        pub artist_button: TemplateChild<gtk::LinkButton>,
+
+        #[template_child]
+        pub artist_button_label: TemplateChild<gtk::Label>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for AlbumDetailsWidget {
+        const NAME: &'static str = "AlbumDetailsWidget";
+        type Type = super::AlbumDetailsWidget;
+        type ParentType = gtk::Box;
+
+        fn class_init(klass: &mut Self::Class) {
+            Self::bind_template(klass);
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for AlbumDetailsWidget {}
+    impl WidgetImpl for AlbumDetailsWidget {}
+    impl BoxImpl for AlbumDetailsWidget {}
 }
 
-impl DetailsWidget {
+glib::wrapper! {
+    pub struct AlbumDetailsWidget(ObjectSubclass<imp::AlbumDetailsWidget>) @extends gtk::Widget, gtk::Box;
+}
+
+impl AlbumDetailsWidget {
     fn new() -> Self {
         screen_add_css_provider(resource!("/components/details.css"));
-        Self::from_resource(resource!("/components/details.ui")).unwrap()
+        glib::Object::new(&[]).expect("Failed to create an instance of AlbumDetailsWidget")
+    }
+
+    fn widget(&self) -> &imp::AlbumDetailsWidget {
+        imp::AlbumDetailsWidget::from_instance(self)
+    }
+
+    fn connect_liked<F>(&self, f: F)
+    where
+        F: Fn() + 'static,
+    {
+        self.widget().like_button.connect_clicked(move |_| f());
+    }
+
+    fn connect_artist_clicked<F>(&self, f: F)
+    where
+        F: Fn() + 'static,
+    {
+        self.widget().artist_button.connect_activate_link(move |_| {
+            f();
+            glib::signal::Inhibit(true)
+        });
+    }
+
+    fn album_tracks_widget(&self) -> &gtk::ListBox {
+        self.widget().album_tracks.as_ref()
+    }
+
+    fn set_liked(&self, is_liked: bool) {
+        self.widget()
+            .like_button
+            .set_label(if is_liked { "♥" } else { "♡" });
     }
 
     fn set_loaded(&self) {
-        let context = self.root.style_context();
+        let context = self.style_context();
         context.add_class("details--loaded");
+    }
+
+    fn set_album_and_artist(
+        &self,
+        album: &str,
+        artist: &str,
+        art_url: Option<&String>,
+        worker: &Worker,
+    ) {
+        let widget = self.widget();
+
+        widget.album_label.set_label(album);
+        widget.artist_button_label.set_label(artist);
+
+        let weak_self = self.downgrade();
+        if let Some(art_url) = art_url.cloned() {
+            worker.send_local_task(async move {
+                if let Some(_self) = weak_self.upgrade() {
+                    let pixbuf = ImageLoader::new()
+                        .load_remote(&art_url[..], "jpg", 100, 100)
+                        .await;
+                    _self.widget().album_art.set_from_pixbuf(pixbuf.as_ref());
+                    _self.set_loaded();
+                }
+            });
+        } else {
+            self.set_loaded();
+        }
     }
 }
 
 pub struct Details {
     model: Rc<DetailsModel>,
     worker: Worker,
-    widget: DetailsWidget,
+    widget: AlbumDetailsWidget,
     children: Vec<Box<dyn EventListener>>,
 }
 
@@ -45,14 +147,15 @@ impl Details {
             model.load_album_info();
         }
 
-        let widget = DetailsWidget::new();
-        let playlist = Box::new(Playlist::new(widget.album_tracks.clone(), model.clone()));
+        let widget = AlbumDetailsWidget::new();
+        let playlist = Box::new(Playlist::new(
+            widget.album_tracks_widget().clone(),
+            model.clone(),
+        ));
 
-        widget
-            .like_button
-            .connect_clicked(clone!(@weak model => move |_| {
-                model.toggle_save_album();
-            }));
+        widget.connect_liked(clone!(@weak model => move || {
+            model.toggle_save_album();
+        }));
 
         Self {
             model,
@@ -65,9 +168,7 @@ impl Details {
     fn update_liked(&self) {
         if let Some(info) = self.model.get_album_info() {
             let is_liked = info.is_liked;
-            self.widget
-                .like_button
-                .set_label(if is_liked { "♥" } else { "♡" });
+            self.widget.set_liked(is_liked);
         }
     }
 
@@ -76,35 +177,20 @@ impl Details {
             let album = &info.title[..];
             let artist = &info.artists_name();
 
-            self.widget.album_label.set_label(album);
-            self.widget.artist_button_label.set_label(artist);
-            let weak_model = Rc::downgrade(&self.model);
-            self.widget.artist_button.connect_activate_link(move |_| {
-                if let Some(model) = weak_model.upgrade() {
+            self.widget.set_liked(info.is_liked);
+            self.widget
+                .set_album_and_artist(album, artist, info.art.as_ref(), &self.worker);
+            self.widget
+                .connect_artist_clicked(clone!(@weak self.model as model => move || {
                     model.view_artist();
-                }
-                glib::signal::Inhibit(true)
-            });
-
-            let widget = self.widget.clone();
-            if let Some(art) = info.art.clone() {
-                self.worker.send_local_task(async move {
-                    let pixbuf = ImageLoader::new()
-                        .load_remote(&art[..], "jpg", 100, 100)
-                        .await;
-                    widget.album_art.set_from_pixbuf(pixbuf.as_ref());
-                    widget.set_loaded();
-                });
-            } else {
-                widget.set_loaded();
-            }
+                }));
         }
     }
 }
 
 impl Component for Details {
     fn get_root_widget(&self) -> &gtk::Widget {
-        &self.widget.root
+        self.widget.upcast_ref()
     }
 
     fn get_children(&mut self) -> Option<&mut Vec<Box<dyn EventListener>>> {
@@ -119,7 +205,6 @@ impl EventListener for Details {
                 if id == &self.model.id =>
             {
                 self.update_details();
-                self.update_liked();
             }
             AppEvent::BrowserEvent(BrowserEvent::AlbumSaved(id))
             | AppEvent::BrowserEvent(BrowserEvent::AlbumUnsaved(id))
