8de3e64d342aa2dde6888d78126d4fae574043f9
Alexandre Trendel <trendel.alexandre@gmail.com>
2021-08-09 18:19:03 +0000

    #267: remove config.rs (#269)

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub static PKGDATADIR: &str = "/home/totaldarkness/.local/share/spot";
-pub static VERSION: &str = "0.1.15-dev";
-pub static LOCALEDIR: &str = "/home/totaldarkness/.local/share/locale";
-pub static APPID: &str = "dev.alextren.Spot.Devel";
