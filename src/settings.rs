7cd82738959b2515670e65e6049df63ebca8f1d6
jannuary <27908024+jannuary@users.noreply.github.com>
2021-10-27 16:18:33 +0700

    Revert "remove prefer-dark-mode setting"
    This reverts commit 18e69ca0548578ce3e8e1f60e951f450bfd23d3c.

--- src/settings.rs
+++ src/settings.rs
@@ -82,6 +82,7 @@ impl SpotifyPlayerSettings {
 }
 
 pub struct SpotSettings {
+    pub prefers_dark_theme: bool,
     pub player_settings: SpotifyPlayerSettings,
     pub window: WindowGeometry,
 }
@@ -89,7 +90,9 @@ pub struct SpotSettings {
 impl SpotSettings {
     pub fn new_from_gsettings() -> Option<Self> {
         let settings = gio::Settings::new(SETTINGS);
+        let prefers_dark_theme = settings.boolean("prefers-dark-theme");
         Some(Self {
+            prefers_dark_theme,
             player_settings: SpotifyPlayerSettings::new_from_gsettings()?,
             window: WindowGeometry::new_from_gsettings(),
         })
@@ -99,6 +102,7 @@ impl SpotSettings {
 impl Default for SpotSettings {
     fn default() -> Self {
         Self {
+            prefers_dark_theme: true,
             player_settings: Default::default(),
             window: Default::default(),
         }
