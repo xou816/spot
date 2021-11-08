52a802b0cf891f2febb77d2bf909f53df4481c8f
Seioo Inoue <sei0o@live.jp>
2021-11-08 09:59:28 +0900

    Automatically try connecting to access points with different ports in case of timeouts

--- src/player/player.rs
+++ src/player/player.rs
@@ -4,7 +4,7 @@ use futures::stream::StreamExt;
 use librespot::core::authentication::Credentials;
 use librespot::core::config::SessionConfig;
 use librespot::core::keymaster;
-use librespot::core::session::Session;
+use librespot::core::session::{Session, SessionError};
 
 use librespot::protocol::authentication::AuthenticationType;
 
@@ -223,6 +223,8 @@ playlist-modify-public,\
 playlist-modify-private,\
 streaming";
 
+const KNOWN_AP_PORTS: &[u16] = &[80, 443, 4070];
+
 async fn get_access_token_and_expiry_time(
     session: &Session,
 ) -> Result<(String, SystemTime), SpotifyError> {
@@ -240,15 +242,50 @@ async fn create_session(
     credentials: Credentials,
     ap_port: Option<u16>,
 ) -> Result<Session, SpotifyError> {
-    let session_config = SessionConfig {
-        ap_port,
-        ..Default::default()
-    };
-    let result = Session::connect(session_config, credentials, None).await;
-    result.map_err(|e| {
-        dbg!(e);
-        SpotifyError::LoginFailed
-    })
+    match ap_port {
+        Some(ap_port) => {
+            let session_config = SessionConfig {
+                ap_port: Some(ap_port),
+                ..Default::default()
+            };
+            let result = Session::connect(session_config, credentials, None).await;
+            result.map_err(|e| {
+                dbg!(e);
+                SpotifyError::LoginFailed
+            })
+        },
+        None => {
+            let mut ret = None;
+
+            let mut ports: Vec<Option<u16>> = KNOWN_AP_PORTS.iter().map(|port| Some(port.to_owned())).collect();
+            ports.push(None);
+            for port in ports {
+                let session_config = SessionConfig {
+                    ap_port: port,
+                    ..Default::default()
+                };
+                let result = Session::connect(session_config, credentials.clone(), None).await;
+
+                match result {
+                    Ok(session) => {
+                        ret = Some(Ok(session));
+                        break
+                    },
+                    Err(SessionError::IoError(_)) => continue,
+                    Err(SessionError::AuthenticationError(_)) => {
+                        ret = Some(Err(SpotifyError::LoginFailed));
+                        break
+                    }
+                }
+            }
+
+            ret.unwrap_or(Err(SpotifyError::LoginFailed))
+        },
+    }
+
+
+
+
 }
 
 async fn player_setup_delegate(
