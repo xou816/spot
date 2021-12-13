52e69ce4e0944ed3e2b1503c31623e9dc5f1605c
Alexandre Trendel <trendel.alexandre@gmail.com>
2021-12-13 09:15:53 -0500

    update more_from label

--- src/app/components/labels.rs
+++ src/app/components/labels.rs
@@ -4,9 +4,6 @@ lazy_static! {
     // translators: This is part of a contextual menu attached to a single track; this entry allows viewing the album containing a specific track.
     pub static ref VIEW_ALBUM: String = gettext("View album");
 
-    // translators: This is part of a contextual menu attached to a single track; the full text is "More from <artist>".
-    pub static ref MORE_FROM: String = gettext("More from");
-
     // translators: This is part of a contextual menu attached to a single track; the intent is to copy the link (public URL) to a specific track.
     pub static ref COPY_LINK: String = gettext("Copy link");
 
@@ -29,8 +26,17 @@ pub fn add_to_playlist_label(playlist: &str) -> String {
 pub fn n_songs_selected_label(n: usize) -> String {
     // this is just to fool xgettext, it doesn't like macros (or rust for that matter) :(
     if cfg!(debug_assertions) {
-        // translators: This is part of a larger text that says "Add to <playlist name>". This text should be as short as possible.
+        // translators: This shows up when in selection mode. This text should be as short as possible.
         ngettext("{} song selected", "{} songs selected", n as u32);
     }
     ngettext!("{} song selected", "{} songs selected", n as u32, n)
+}
+
+pub fn more_from_label(artist: &str) -> String {
+    // this is just to fool xgettext, it doesn't like macros (or rust for that matter) :(
+    if cfg!(debug_assertions) {
+        // translators: This is part of a contextual menu attached to a single track; the full text is "More from <artist>".
+        gettext("More from {}");
+    }
+    gettext!("More from {}", glib::markup_escape_text(artist))
 }
