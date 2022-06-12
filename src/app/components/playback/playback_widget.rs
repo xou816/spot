c2fc54be8ba82ff8b868e113695ae6ecf3743477
Diego Augusto S. C <diego.augusto@protonmail.com>
2022-06-11 22:06:10 -0300

    Place volume slider under controls

--- src/app/components/playback/playback_widget.rs
+++ src/app/components/playback/playback_widget.rs
@@ -97,7 +97,7 @@ impl PlaybackWidget {
         let weak_self = self.downgrade();
         worker.send_local_task(async move {
             let loader = ImageLoader::new();
-            let result = loader.load_remote(&url, "jpg", 48, 48).await;
+            let result = loader.load_remote(&url, "jpg", 64, 64).await;
             if let (Some(ref _self), Some(ref result)) = (weak_self.upgrade(), result) {
                 _self.set_artwork(result);
             }
