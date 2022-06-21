c431fbff431bbc2e5f8b0f62047558b0f76e1033
Hoang Seidel <hoangseidel02@gmail.com>
2022-06-21 23:04:20 +0200

    nest "More from artist" menu entries in submenu

--- src/app/components/details/details_model.rs
+++ src/app/components/details/details_model.rs
@@ -204,11 +204,27 @@ impl PlaylistModel for DetailsModel {
         let song = song.description();
 
         let menu = gio::Menu::new();
-        for artist in song.artists.iter() {
-            menu.append(
-                Some(&labels::more_from_label(&artist.name)),
-                Some(&format!("song.view_artist_{}", artist.id)),
-            );
+
+        let artists_iter = song.artists.iter();
+
+        if artists_iter.len() > 1 {
+            let submenu = gio::Menu::new();
+
+            for artist in artists_iter {
+                submenu.append(
+                    Some(&artist.name),
+                    Some(&format!("song.view_artist_{}", artist.id)),
+                );
+            }
+
+            menu.append_submenu(Some("More from"), &submenu);
+        } else {
+            for artist in artists_iter {
+                menu.append(
+                    Some(&labels::more_from_label(&artist.name)),
+                    Some(&format!("song.view_artist_{}", artist.id)),
+                );
+            }
         }
 
         menu.append(Some(&*labels::COPY_LINK), Some("song.copy_link"));
